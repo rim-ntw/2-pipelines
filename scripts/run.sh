3adbf1827e4db8e0aef054c5ac4ede4c886666d0
rim-ntw <71330911+rim-ntw@users.noreply.github.com>
2020-10-01 14:58:09 +0100

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-sudo docker run -d -p 5000:5000 --name flask-app-demo-2 localhost:8082/flask-app-demo-2
+sudo docker run -d -p 5001:5000 --name flask-app-demo-2 localhost:8082/flask-app-demo-2
